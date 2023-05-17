8a19f73fa0043ada2c4f6e3477e0807d591a9e4d
SEANYB4 <bainsean@outlook.com>
2023-05-17 10:47:08 +0800

    Added scanner

--- main.go
+++ main.go
@@ -2,11 +2,18 @@ package main
 
 import (
 	"fmt"
+	"bufio"
+	"os"
 )
 
 
 func main() {
 
+	fmt.Println("type something")
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	text := scanner.Text()
 
-	fmt.Println("hello world")
+
+	fmt.Println("echoing: ", text)
 }
